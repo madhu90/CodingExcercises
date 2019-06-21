62e7d85654dd820be34b86f00cc0a023270048e3
madhu90 <madhumitha.raghu1106@gmail.com>
2019-06-21 15:02:15 -0400

    Accounting for different number of digits

--- karatsubaMultiplication.cpp
+++ karatsubaMultiplication.cpp
@@ -5,28 +5,37 @@
 #include <algorithm>
 #include <math.h>
 
+// If x and y have same lengths
 // x = (10^n/2)*a + b
 // y = (10^n/2)*c + d
 // x*y = ((10^n/2)*a + b)*((10^n/2)*c + d)
 //     = (10^n)*ac + (10^n/2)*(ad + bc) + bd
+
+// If x and y have different lengths
+// x = (10^n/2)*a + b
+// y = (10^m/2)*c + d
+// x*y = ((10^n/2)*a + b)*((10^m/2)*c + d)
+//     = (10^(n+m)/2)*ac + (10^n/2)*ad +  (10^m/2)*bc + bd
 
 class Karatsuba
 {
 private:
     unsigned int f1;
     unsigned int f2;
-    unsigned int numdigits;
+    unsigned int numdigits1;
+    unsigned int numdigits2;
 
 public:
     Karatsuba(unsigned int val1, unsigned int val2)
         : f1(val1), f2(val2)
     {
-        numdigits = std::to_string(std::max(f1, f2)).length();
+        numdigits1 = std::to_string(f1).length();
+        numdigits2 = std::to_string(f2).length();
     }
 
     unsigned int computeProduct(void)
     {
-        return compute(f1, f2, numdigits);
+        return compute(f1, f2, numdigits1, numdigits2);
     }
 
     void printLookupContents()
@@ -39,60 +48,65 @@ public:
     }
 
 private:
-    unsigned int compute(unsigned int val1, unsigned int val2, unsigned int numdigits)
-    {
-        if (numdigits == 1)
-            return val1 * val2;
-
-        unsigned int newdigits = numdigits / 2;
-        unsigned int a = val1 / std::pow(10, newdigits);
-        unsigned int b = val1 % static_cast<unsigned int>(std::pow(10, newdigits));
-        unsigned int c = val2 / std::pow(10, newdigits);
-        unsigned int d = val2 % static_cast<unsigned int>(std::pow(10, newdigits));
-
-        // Check map if entry exists
+    unsigned int compute(unsigned int val1, 
+        unsigned int val2, 
+        unsigned int m, 
+        unsigned int n)
+    {        
+        unsigned int a = 0;
+        unsigned int b = 0;
+        unsigned int c = 0;
+        unsigned int d = 0;
         unsigned int ac = 0;
         unsigned int bd = 0;
-        unsigned int ad_bc = 0;
+        unsigned int ad = 0;
+        unsigned int bc = 0;        
+        unsigned int new_m = m;
+        unsigned int new_n = n;
 
-        // ac
-        std::string key1 = std::to_string(a) + std::string("*") + std::to_string(c);
-        if (lookup.find(key1) == lookup.end()) {
-            lookup[key1] = compute(a, c, newdigits);
-            ac = lookup[key1];
+        if (new_m == 1 && new_n == 1) {
+            return val1 * val2;
+        } else if (new_m == 1) {
+            // Split y   
+            a = val1;
+            b = 1;
+            splitData(c, d, val2, new_n);            
+        } else if (new_n == 1) {
+            // Split x  
+            c = val2;
+            d = 1;
+            splitData(a, b, val1, new_m);            
         } else {
-            ac = lookup[key1];
+            // Split x and y  
+            splitData(a, b, val1, new_m);
+            splitData(c, d, val2, new_n);
         }
+         
+        // ac
+        ac = compute(a, c, new_m, new_n);
 
         // bd
-        std::string key2 = std::to_string(b) + std::string("*") + std::to_string(d);
-        if (lookup.find(key2) == lookup.end()) {
-            lookup[key2] = compute(b, d, newdigits);
-            bd = lookup[key2];
-        } else {
-            bd = lookup[key2];
-        }
+        bd = compute(b, d, new_m, new_n);
 
         // ad + bc
-        std::string key3 = std::to_string(a) + std::to_string(d) + std::string("*") + std::to_string(b) + std::to_string(c);
-        if (lookup.find(key3) == lookup.end()) {
-            lookup[key3] = compute((a + b), (c + d), newdigits) - ac - bd;
-            ad_bc = lookup[key3];
-        } else {
-            ad_bc = lookup[key3];
-        }
+        ad = compute(a, d, new_m, new_n);
+        bc = compute(b, c, new_m, new_n);        
 
-        return std::pow(10, numdigits)*ac + std::pow(10, newdigits)*ad_bc + bd;
+        return std::pow(10, (m + n)/2)*ac + std::pow(10, n/ 2)*ad + std::pow(10, m / 2)*bc + bd;        
     }
 
-private:
-    std::unordered_map<std::string, unsigned int> lookup;
+    void splitData(unsigned int& c, unsigned int &d, unsigned int val, unsigned int& n)
+    {
+        n = n / 2;
+        c = val / std::pow(10, n);
+        d = val % static_cast<unsigned int>(std::pow(10, n));
+    }
 };
 
 int main()
 {
-    unsigned int value1 = 1234;
-    unsigned int value2 = 5678;
+    unsigned int value1 = 12345;
+    unsigned int value2 = 56783;
     Karatsuba calc(value1, value2);    
     unsigned int result = calc.computeProduct();
     std::cout << "Result of " << value1 << "*" << value2 << "   " << result << std::endl;
